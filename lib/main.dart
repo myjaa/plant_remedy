c8bc9febc8b8d350999dfb9f07a59b745267761f
Mohammad Yusuf Jamal Aziz Azmi <yusufjamal2773@gmail.com>
2020-11-14 09:52:34 +0530

    Added the remedies.

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,21 @@ class _DetectMainState extends State<DetectMain> {
   double _imageHeight;
   var _recognitions;
   var remedy = {
-    'Cardamom': ['remedy1C\ntest', 'remedy2C'],
-    'Gotu Kola': ['remedy1GK'],
-    'Turmeric': ['remedy1T']
+    'Cardamom': [
+      'REGULARISING HEART RATE\n\nAdd a teaspoonful of cardamom powder to a little honey and have it once or twice a day for the above said benefits.',
+      'DIGESTIVE HEALTH\n\nyou can consume the seeds plain, sprinkle ground cardamom on your food or go for a healthy cup of cardamom tea.',
+      'COLD and FLU\n\nCardamom tea twice a day when you have symptoms of cold is considered ideal'
+    ],
+    'Gotu Kola': [
+      ' MAY HELP IN IMPROVING COGNITIVE FUNCTIONS\n\nAlthough gotu kola and folic acid were equally beneficial in improving overall cognition, gotu kola was more effective in improving memory domain.How to use: Take 750 to 1,000 mg of gotu kola per day for up to 14 days at a time.',
+      'MAY REDUCE ANXIETY & STRESS\n\n Sleep deprivation can cause anxiety, oxidative damage, and neuroinflammation.How to use: Take 500 mg of gotu kola extract twice a day for up to 14 days at a time. You can take up to 2,000 mg per day in cases of extreme anxiety.'
+    ],
+    'Turmeric': [
+      'CUTS, BRUISES & WOUNDS\n\nIf you’ve cut your finger while learning to cook, chances are that someone immediately took out the spice box to put some haldi powder on it. Its antiseptic qualities - some of the health benefits of turmeric - are the reason that this remedy has been effective for generations.',
+      'DIGESTION\n\nTurmeric not only helps in building an appetite, it also improves digestion. It helps stimulates the secretion of digestive juices which are essential for proper functioning of the entire digestive system. Hot tea made with turmeric paste, ginger, lemon, and honey is good way to consume turmeric for this benefit',
+      'COLD & COUGH\n\nHaldi doodh is a concoction often recommended by elders to tackle the seasonal colds and coughs. To make this drink, spices such as cardamom, pepper, ginger, and cinnamon are added to boiling milk with haldi powder. Honey can also be added as it helps soothe a sore throat. Turmeric’s anti-inflammatory, antiseptic and expectorant properties empower us to fight cold and chest ailments speedily. Turmeric in warm milk is a timeless home remedy.',
+      'ACNE & SKIN TROUBLES\n\nA face pack made of besan (gram flour) along with haldi powder and milk can help in reducing the appearance of acne and its scars. This face pack can also help brighten skin and reduce uneven skin tone. Antiseptic, anti-oxidant and anti-inflammatory properties of turmeric provide this benefit. This turmeric pack is a great home remedy for dull skin'
+    ]
   };
 
   loadModel() async {
@@ -116,26 +128,29 @@ class _DetectMainState extends State<DetectMain> {
     }
 
     var remedyList = remedy[_recognitions[0]['label'].toString()];
+    // var remedyList=remedy['Turmeric'];
     print(remedyList.length);
     print(remedyList);
 
     return ListView.builder(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       itemCount: remedyList.length,
       itemBuilder: (context, index) {
         return Container(
             padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-            height: 220,
+            height: 500,
             width: double.maxFinite,
             child: Card(
+                color: Colors.lightGreen,
                 elevation: 5,
                 // child: Center(
                 child: Padding(
-                    padding: EdgeInsets.fromLTRB(15, 20, 15, 15),
+                    padding: EdgeInsets.fromLTRB(20, 50, 15, 15),
                     child: Text(
-                      "Remedy for " + remedyList[index],
+                      remedyList[index],
                       style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
                     )))
             // )
             );
@@ -159,6 +174,7 @@ class _DetectMainState extends State<DetectMain> {
         child: Text(
           "Prediction: " + _recognitions[0]['label'].toString().toUpperCase(),
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+          overflow: TextOverflow.ellipsis,
         ),
       ),
     );
